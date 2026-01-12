594b65edfe2438be6926a25ecc47bf8239ecc032
Muhammad Ahmer <b00087698@alumni.aus.edu>
2026-01-12 23:42:29 +0400

    match case statement

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import calculator
 print("Welcome to CLI Calculator!", "Enter 'exit' to quit.", sep='\n')
 
 user_input = 0
-while user_input != "exit":
-    print("Enter calculation: ")
-    user_input = input()
+while True:
+    user_input = input("Enter calculation: ")
+
+    if not user_input != "exit":
+        break
+
     user_input = user_input.split(" ")
+
+    if len(user_input) != 3:
+        print("Invalid input format. Use: number operator number")
+        continue
 
     match user_input[1]:
         case '+':
-            calculator.add(user_input[0], user_input[2])
+            calc_output = calculator.add(float(user_input[0]), float(user_input[2]))
         case '-':
-            calculator.subtract(user_input[0], user_input[2])
-        case '+':
-            calculator.multiply(user_input[0], user_input[2])
+            calc_output = calculator.subtract(float(user_input[0]), float(user_input[2]))
+        case '*':
+            calc_output = calculator.multiply(float(user_input[0]), float(user_input[2]))
         case '/':
-            calculator.divide(user_input[0], user_input[2])
+            calc_output = calculator.divide(float(user_input[0]), float(user_input[2]))
+        case _:
+            print("Invalid Operation")
+            calc_output = ""
 
-
-
+    if calc_output != "":
+        print(calc_output)
